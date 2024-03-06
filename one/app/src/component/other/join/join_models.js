f750b0a2f6e9c3fadd40f07059d63a51799fcaaa
Hrothnar <hrothnar@mail.ru>
2024-03-06 12:32:04 +0400

    Small fixes

---
+++ one/app/src/component/other/join/join_models.js
@@ -0,0 +1,37 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../../../configuration/db_config.js";
+
+class Taggable extends Model {
+
+};
+
+Taggable.init({
+    tagId: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        unique: true,
+        autoIncrement: true,
+        field: "tag_id",
+        primaryKey: true,
+    },
+    taggableType: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: false,
+        field: "taggable_type",
+    },
+    taggableId: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        unique: false,
+        field: "taggable_id",
+        references: null,
+    },
+}, {
+    sequelize: sequelize,
+    timestamps: false,
+    modelName: "Taggable",
+    tableName: "taggables",
+});
+
+export { Taggable };
